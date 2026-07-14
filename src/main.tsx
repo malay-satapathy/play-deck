a5a3fec73afa91d0ada09219dea8a74532d7958c
Malay Satapathy <satapathymalay.95@gmail.com>
2026-07-14 18:53:28 +0530

    fix: routing and context providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import { GlobalProvider } from './core/store/GlobalContext'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GlobalProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </GlobalProvider>
   </StrictMode>,
 )
